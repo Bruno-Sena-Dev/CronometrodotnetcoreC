f9219dd9e31f77a34da2c60423cff375d6161930
person2017 <brunoperson2015@gmail.com>
2024-07-29 23:05:38 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Menu();
+    }
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundos ");
+        Console.WriteLine("M = Minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar?");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data.Substring(0, data.Length - 1));
+
+        int multiplier = 1;
+
+        if (type == 'm')
+
+            multiplier = 60;
+        if (time == 0)
+            System.Environment.Exit(0);
+        PreStart(time * multiplier);
+    }
+    static void Start(int time)
+    {
+
+        int currentTime = 0;
+
+        while (time != currentTime)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("EstopWatch finalizado!");
+        Thread.Sleep(2500);
+        Menu();
+    }
+
+    static void PreStart(int time)
+    {
+        Console.Clear();
+        Console.WriteLine("Ready......");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set......");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go...");
+        Thread.Sleep(2500);
+
+        Start(time);
+    }
+}
